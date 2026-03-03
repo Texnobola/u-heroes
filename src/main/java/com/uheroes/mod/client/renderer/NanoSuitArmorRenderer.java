267fcbb173a71cdb9d47def4d25c689d2a294c88
unknown <{texnobola2@gmail.com}>
2026-03-03 21:03:04 +0500

    kinda-working

--- src/main/java/com/uheroes/mod/client/renderer/NanoSuitArmorRenderer.java
+++ src/main/java/com/uheroes/mod/client/renderer/NanoSuitArmorRenderer.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.uheroes.mod.heroes.nanotech.armor.NanoSuitArmorItem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
@@ -27,32 +29,6 @@ public class NanoSuitArmorRenderer extends GeoArmorRenderer<NanoSuitArmorItem> {
                 return null;
             }
         });
-    }
-    
-    @Override
-    public ResourceLocation getModelLocation(NanoSuitArmorItem animatable) {
-        if (this.currentStack == null)
-            return new ResourceLocation("u_heroes", "geo/nanosuit_helmet.geo.json");
-        return switch (this.currentStack.getEquipmentSlot()) {
-            case HEAD -> new ResourceLocation("u_heroes", "geo/nanosuit_helmet.geo.json");
-            case CHEST -> new ResourceLocation("u_heroes", "geo/nanosuit_chestplate.geo.json");
-            case LEGS -> new ResourceLocation("u_heroes", "geo/nanosuit_leggings.geo.json");
-            case FEET -> new ResourceLocation("u_heroes", "geo/nanosuit_boots.geo.json");
-            default -> new ResourceLocation("u_heroes", "geo/nanosuit_helmet.geo.json");
-        };
-    }
-    
-    @Override
-    public ResourceLocation getTextureLocation(NanoSuitArmorItem animatable) {
-        if (this.currentStack == null)
-            return new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_helmet.png");
-        return switch (this.currentStack.getEquipmentSlot()) {
-            case HEAD -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_helmet.png");
-            case CHEST -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_chestplate.png");
-            case LEGS -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_leggings.png");
-            case FEET -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_boots.png");
-            default -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_helmet.png");
-        };
     }
     
     @Override
