6fb582e1cdd9750d61e6e801ac3cd552a7b78de2
unknown <{texnobola2@gmail.com}>
2026-03-07 17:56:40 +0500

    pushes

--- src/main/java/com/uheroes/mod/client/renderer/NanoSuitArmorRenderer.java
+++ src/main/java/com/uheroes/mod/client/renderer/NanoSuitArmorRenderer.java
@@ -4,39 +4,36 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.uheroes.mod.heroes.nanotech.armor.NanoSuitArmorItem;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
-/**
- * GeckoLib renderer for Nano-Tech Suit armor.
- */
 public class NanoSuitArmorRenderer extends GeoArmorRenderer<NanoSuitArmorItem> {
+
     public NanoSuitArmorRenderer() {
         super(new GeoModel<NanoSuitArmorItem>() {
             @Override
             public ResourceLocation getModelResource(NanoSuitArmorItem animatable) {
-                return new ResourceLocation("u_heroes", "geo/nanosuit_helmet.geo.json");
+                return switch (animatable.getType()) {
+                    case HELMET     -> new ResourceLocation("u_heroes", "geo/nanosuit_helmet.geo.json");
+                    case CHESTPLATE -> new ResourceLocation("u_heroes", "geo/nanosuit_chestplate.geo.json");
+                    case LEGGINGS   -> new ResourceLocation("u_heroes", "geo/nanosuit_leggings.geo.json");
+                    case BOOTS      -> new ResourceLocation("u_heroes", "geo/nanosuit_boots.geo.json");
+                };
             }
             @Override
             public ResourceLocation getTextureResource(NanoSuitArmorItem animatable) {
-                return new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_helmet.png");
+                return switch (animatable.getType()) {
+                    case HELMET     -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_helmet.png");
+                    case CHESTPLATE -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_chestplate.png");
+                    case LEGGINGS   -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_leggings.png");
+                    case BOOTS      -> new ResourceLocation("u_heroes", "textures/entity/armor/nanosuit_boots.png");
+                };
             }
             @Override
             public ResourceLocation getAnimationResource(NanoSuitArmorItem animatable) {
                 return null;
             }
         });
-    }
-    
-    @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.getCameraEntity() == null || mc.level == null) return;
-        try {
-            super.renderToBuffer(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        } catch (NullPointerException ignored) {}
     }
 }
